f7ef818ea910ec315a058f65a845c7c42cfcf54f
ward233 <1095100728@qq.com>
2016-10-05 15:22:19 +0800

    增加了些注释

--- js/index.js
+++ js/index.js
@@ -1,28 +1,56 @@
 $(document).ready(function() {
+    /**
+     * 该类用于封装增强了的localStorage对象方法
+     */
+    function LocalStorage() {
 
-    function LocalStorage() {}
-    LocalStorage.prototype.getUserList = function() {
-        var userList = [];
-        for (var i = 0; i < localStorage.length; i++) {
-            userList.push(localStorage.key(i));
-        }
-        return userList;
-    };
+        /**
+         * 从本地存储中得到联系人列表
+         * @return {Array} 由联系人名构成的数组
+         */
+        this.getUserList = function() {
+            var userList = [];
+            for (var i = 0; i < localStorage.length; i++) {
+                userList.push(localStorage.key(i));
+            }
+            return userList;
+        };
 
-    LocalStorage.prototype.setParseItem = function(itemName, itemObjContent) {
-        localStorage.setItem(itemName, JSON.stringify(itemObjContent));
-    };
 
-    LocalStorage.prototype.getParseItem = function(itemName) {
-        return $.parseJSON(localStorage[itemName]);
-    };
-    LocalStorage.prototype.removeItem = function(itemName) {
-        localStorage.removeItem(itemName);
-    };
+        /**
+         * 将json对象存储为json字面量
+         * @param {String} itemName       json对象的标示名 这里为联系人的名字
+         * @param {Object} itemObjContent 要存放的json对象
+         */
+        this.setParseItem = function(itemName, itemObjContent) {
+            localStorage.setItem(itemName, JSON.stringify(itemObjContent));
+        };
+
+
+        /**
+         * 得到由json字面量转化而来的json对象
+         * @param  {String} itemName 联系人的名字
+         * @return {Object}          要取出的json对象
+         */
+        this.getParseItem = function(itemName) {
+            return $.parseJSON(localStorage[itemName]);
+        };
+
+
+        /**
+         * 删除某个联系人的信息
+         * @param  {String} itemName 联系人名
+         */
+        this.removeItem = function(itemName) {
+            localStorage.removeItem(itemName);
+        };
+    }
+
 
     var userLocalInfo = new LocalStorage();
 
 
+   //主页的Vue实例
     var mainVm = new Vue({
         el: '#main',
         data: {
@@ -30,37 +58,44 @@ $(document).ready(function() {
             userList: userLocalInfo.getUserList()
         },
         methods: {
+
+            //显示联系人详细信息
             showUserContactInfo: function(event) {
                 this.show = false;
                 userContactInfoVm.show = true;
-                // userContactInfoVm.userInfo = $.parseJSON(localStorage[$.trim($(event.target).text())]);
+
+                //将本地数据载入到详细信息里的userInfo对象中
                 userContactInfoVm.userInfo = userLocalInfo.getParseItem($.trim($(event.target).text()));
             },
+            //显示新建联系人页面
             showNewContact: function() {
                 this.show = false;
                 newContactVm.show = true;
             }
         },
+        //在Vue实例创建完毕后 再显示该实例 防止在加载中出现画面抖动
         ready: function() {
             $(this.$el).css('visibility', 'visible');
         }
 
     });
 
+    //新联系人页的Vue实例
     var newContactVm = new Vue({
         el: '#new-contact',
         data: {
             show: false,
-            dataNum: 0,
+            dataNum: 0, //用于添加号码时的计数, 标识不同的号码
             userInfo: {
                 userName: "",
-                phoneList: [],
+                phoneList: [], // 该数组是对象数组 每个对象有三个属性 dataNum中存放 号码的标识 phoneType中存放 号码的类型 phoneNum中存放号码的值
                 phoneRing: "默认",
                 phoneShock: "默认"
             }
 
         },
         methods: {
+            //初始化用户信息
             initInfo: function() {
                 this.userInfo.userName = "";
                 this.dataNum = 0;
@@ -68,6 +103,7 @@ $(document).ready(function() {
                 this.userInfo.phoneRing = "默认";
                 this.userInfo.phoneShock = "默认";
             },
+
             addPhone: function() {
                 this.userInfo.phoneList.push({
                     dataNum: this.dataNum,
@@ -77,7 +113,9 @@ $(document).ready(function() {
                 this.dataNum += 1;
             },
             removePhone: function(event) {
+                // 获取到事件发生时的那个li 的dataNum
                 var dataNum = $(event.target).parent().parent().attr('data-num');
+                // 将有这个dataNum的对象从phoneList中筛出
                 this.userInfo.phoneList = this.userInfo.phoneList.filter(function(item) {
                     return item.dataNum !== parseInt(dataNum);
                 });
@@ -91,13 +129,16 @@ $(document).ready(function() {
 
             },
             save: function() {
+                //名字为空或者名字中有空格
                 if (this.userInfo.userName.length === 0 || this.userInfo.userName.indexOf(" ") !== -1) {
                     alert("名字不可为空！");
                 } else {
+                    //将usefInfo 中存放的用户数据放到本地存储中
                     userLocalInfo.setParseItem(this.userInfo.userName, this.userInfo);
                     this.initInfo();
                     this.show = false;
                     mainVm.show = true;
+                    //更新首页列表
                     mainVm.userList = userLocalInfo.getUserList();
                 }
             }
